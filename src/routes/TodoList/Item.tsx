7e9c4e3d50e493acd96089485a2930d0f33ad3e1
Lee Jun Hyuk <miriya.lee@gmail.com>
2022-05-10 22:03:30 +0900

    use more css way

--- src/routes/TodoList/Item.tsx
+++ src/routes/TodoList/Item.tsx
@@ -2,7 +2,7 @@ import { ChangeEvent } from 'react'
 import cx from 'classnames'
 import styles from './TodoList.module.scss'
 
-import { useEffect, useState, usePrevious, useTimeoutFn } from 'hooks'
+// import { useEffect, useState, usePrevious, useTimeoutFn } from 'hooks'
 import { useRecoil } from 'hooks/state'
 import { todoListState } from 'states/todo'
 import { ITodoItem } from 'types/todo.d'
@@ -15,8 +15,8 @@ interface Props {
 
 const TodoItem = ({ todo }: Props) => {
   const { done, id, title } = todo
-  const prevDone = usePrevious(done)
-  const [isDone, setIsDone] = useState(done)
+  // const prevDone = usePrevious(done)
+  // const [isDone, setIsDone] = useState(done)
 
   const [, setTodoList] = useRecoil(todoListState)
 
@@ -28,19 +28,20 @@ const TodoItem = ({ todo }: Props) => {
     })
   }
 
-  const [plantSetIsDone] = useTimeoutFn(() => setIsDone(true), 500)
+  // const [plantSetIsDone] = useTimeoutFn(() => setIsDone(true), 500)
 
-  useEffect(() => {
-    if (prevDone === undefined || prevDone === done) return
-    if (done) {
-      plantSetIsDone()
-    } else {
-      setIsDone(done)
-    }
-  }, [done, plantSetIsDone, prevDone])
+  // useEffect(() => {
+  //   if (prevDone === undefined || prevDone === done) return
+  //   if (done) {
+  //     plantSetIsDone()
+  //   } else {
+  //     setIsDone(done)
+  //   }
+  // }, [done, plantSetIsDone, prevDone])
 
   return (
-    <li className={cx(styles.task, { [styles.done]: isDone })}>
+    // <li className={cx(styles.task, { [styles.done]: isDone })}>
+    <li className={cx(styles.task, { [styles.done]: done })}>
       <div className={styles.checkboxWrapper}>
         <input type='checkbox' checked={done} onChange={handleChange} />
         <CheckIcon />
